df4392888169d332f112d1fe46b3f20db927bfbc
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-27 13:38:10 -0300

    Criando rota de categoria

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running!");
