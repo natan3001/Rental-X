43167079775e244b21738d43cf09bcce8a43c601
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-27 14:20:14 -0300

    Criando repositório de categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,26 @@
+import { Category } from "../models/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO) {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
