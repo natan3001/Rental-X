f3c5a640f4c10f8931dd6ada0e44db3a7f1bc8f8
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-28 19:26:13 -0300

    Criado o repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,21 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 export class CreateSpecificationService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ description, name }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists");
+    }
+
+    this.specificationRepository.create({ name, description });
   }
 }
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  response.status(201).send();
+});
+
+export { specificationsRoutes };
