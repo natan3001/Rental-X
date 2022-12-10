7ba5530a730390a1fba2662257cf8eba9f4bd762
Natan Martins dos Santos <natantcr@gmail.com>
2022-12-10 16:25:52 -0300

    Refatorando a useCase de criar categoria

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -5,11 +5,14 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 export class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-  handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response) {
     const { name, description } = request.body;
 
-    this.createCategoryUseCase.execute({ name, description });
-
-    return response.status(201).send();
+    try {
+      await this.createCategoryUseCase.execute({ name, description });
+      return response.status(201).send();
+    } catch (error) {
+      return response.status(400).json({ message: error.message });
+    }
   }
 }
