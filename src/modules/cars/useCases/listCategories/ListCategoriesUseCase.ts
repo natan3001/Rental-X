adedc17b95000efb2b1a480ca469803e73790e1d
Natan Martins dos Santos <natantcr@gmail.com>
2022-12-12 20:16:28 -0300

    Refatorado o useCase de listagem de categorias

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,7 +1,13 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+@injectable()
 export class ListCategoriesUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
   async execute() {
     const categories = await this.categoriesRepository.list();
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
-import listCategoriesController from "../modules/cars/useCases/listCategories";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 
@@ -12,13 +12,13 @@ const upload = multer({
 });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
+
 categoriesRoutes.post("/", createCategoryController.handle);
 
-categoriesRoutes.get("/", (request, response) => {
-  return listCategoriesController().handle(request, response);
-});
+categoriesRoutes.get("/", listCategoriesController.handle);
 
-const importCategoryController = new ImportCategoryController();
 categoriesRoutes.post(
   "/import",
   upload.single("file"),
